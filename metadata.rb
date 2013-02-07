ecfb2a713d9f10ba2a187be6e7776395b69a8839
sawanoboly <sawanoboriyu@higanworks.com>
2013-02-08 01:04:17 +0900

    initial

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "chef_client_smf"
+maintainer       "HiganWorks LLC"
+maintainer_email "sawanoboriyu@higanwoks.com"
+license          "MIT"
+description      "chef-client run under smf"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+depends          "smf"
+supports         "smartos"
